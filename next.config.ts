ca4dfb4664a9448dbb83e30cd349255cf536d9da
1F10230245 <s1f102302450@iniad.org>
2025-11-19 14:48:09 +0900

    login

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    // X(Twitter)とGoogleのプロフィール画像を表示するために追加
+    domains: ["pbs.twimg.com", "lh3.googleusercontent.com"],
+  },
 };
 
 export default nextConfig;
